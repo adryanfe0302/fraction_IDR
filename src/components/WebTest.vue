a018f5f2b4acd56dd503bab3aa5608cb25deabf3
adryan <dryan_guns@yahoo.co.id>
2020-09-24 22:43:13 +0700

    test 1

--- src/components/WebTest.vue
+++ src/components/WebTest.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container">
-  // test
+  // test 1
   <div class="master">
     <span for="name"><b>IDR Amount</b></span>
     <input v-model="valueAmount" type="text" placeholder="Enter Amount" name="name" required>
